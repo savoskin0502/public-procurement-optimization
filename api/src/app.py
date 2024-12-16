3b1be6e3bb25ef4142282203d0172a0ee4368922
Savoskin Roman <savoskin0500@gmail.com>
2024-12-16 15:39:26 +0500

    add api for test

---
+++ api/src/app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title("Win More Tenders with Price Recommendations")
+
+st.markdown(
+    """
+    <p style='text-align: left; color: gray; font-size: 17px; margin-top: -10px;'>
+        Simplify public procurement pricing. Input a lot number and receive an expertly calculated price recommendation
+    </p>
+    """,
+    unsafe_allow_html=True,
+)
